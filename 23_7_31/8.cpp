4aee24c7901dd8b5aa61e6e601f3025e3f983fae
aire_233 <chenyimi21@mails.tsinghua.edu.cn>
2023-07-31 23:52:43 +0800

    Day 1

---
+++ 23_7_31/8.cpp
@@ -0,0 +1,32 @@
+class Solution
+{
+public:
+    void moveZeroes(vector<int> &nums)
+    {
+        int n = nums.size(), index = 0, i = 0;
+        for (; i < n; i++)
+            if (nums[i] != 0)
+                nums[index++] = nums[i];
+        for (; index < n; index++)
+            nums[index] = 0;
+    }
+};
+
+class Solution
+{
+public:
+    void moveZeroes(vector<int> &nums)
+    {
+        int n = nums.size(), i = 0, j = 0;
+        for (; i < n && j < n; j++)
+        {
+            if (nums[j] == 0)
+                i++;
+            else if (i != 0)
+            {
+                nums[j - i] = nums[j];
+                nums[j] = 0;
+            }
+        }
+    }
+};
