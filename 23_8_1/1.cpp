0e5f599bb2164f7250505ac33f74f27576af5fed
aire_233 <chenyimi21@mails.tsinghua.edu.cn>
2023-08-01 09:08:57 +0800

    Day 2

---
+++ 23_8_1/1.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+#include <map>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        int n = nums.size();
+        map<int, int> m;
+        for (int i = 0; i < n; i++)
+        {
+            if (m.find(target - nums[i]) != m.end())
+            {
+                return {m[target - nums[i]], i};
+            }
+            m[nums[i]] = i;
+        }
+        return {};
+    }
+};
