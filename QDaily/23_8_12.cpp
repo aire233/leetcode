80da359274ee7f0da7e2dd80628803f303b88c8b
aire_233 <chenyimi21@mails.tsinghua.edu.cn>
2023-08-27 18:28:38 +0800

    Day 13

--- QDaily/23_8_12.cpp
+++ QDaily/23_8_12.cpp
@@ -17,7 +17,7 @@ public:
     {
         auto cmp = [](ListNode *a, ListNode *b)
         { return a->val > b->val; };
-        priority_queue<ListNode *, vector<ListNode *>, decltype(cmp)> pq;
+        priority_queue<ListNode *, vector<ListNode *>, decltype(cmp)> pq(cmp);
         for (auto list : lists)
         {
             if (list)
