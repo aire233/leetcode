4fb1ad79e49cf51ab1212f2e8053e4f6c97cfca0
aire_233 <chenyimi21@mails.tsinghua.edu.cn>
2023-09-05 15:59:38 +0800

    20230905

---
+++ weekGames/weekGame_361/4.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<int> minOperationsQueries(int n, vector<vector<int>>& edges, vector<vector<int>>& queries) {
+        
+    }
+};
