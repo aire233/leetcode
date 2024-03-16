0fdaf30ab77d4c36059a07d7e444ae3288a9999d
aire233 <ym3rchen@gmail.com>
2024-03-16 13:36:45 +0800

    weekGame 387

---
+++ weekGames/weekGame_387/4.cpp
@@ -0,0 +1,95 @@
+#include "bits/stdc++.h"
+#define For(i, a, b) for (auto i = a; i < (b); ++i)
+#define Rof(i, a, b) for (auto i = a; i > (b); --i)
+#define all(a) a.begin(), a.end()
+#define sz(a) (int)(a).size()
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (b < a) { a = b; return 1; } return 0; }
+using namespace std;
+
+template<class A, class B> string to_string(const pair<A, B>& p);
+template<class A, class B, class C> string to_string(const tuple<A, B, C>& p);
+template<class A, class B, class C, class D> string to_string(const tuple<A, B, C, D>& p);
+string to_string(const string& s) { return '"' + s + '"'; }
+string to_string(const char* s) { return to_string((string)s); }
+string to_string(char c) { return "'" + string(1, c) + "'"; }
+string to_string(bool x) { return x ? "true" : "false"; }
+template<size_t L> string to_string(const bitset<L>& x) { return x.to_string(); }
+template<class A, class T = typename A::value_type> string to_string(const A& v) {
+    bool first = 1;
+    string res = "{";
+    for (const auto& x : v) {
+        if (!first) res += ", ";
+        first = 0;
+        res += to_string(x);
+    }
+    res += "}";
+    return res;
+}
+template<class A, class B> string to_string(const pair<A, B>& p) {
+    return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
+}
+template<class A, class B, class C> string to_string(const tuple<A, B, C>& p) {
+    return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ")";
+}
+template<class A, class B, class C, class D> string to_string(const tuple<A, B, C, D>& p) {
+    return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ", " + to_string(get<3>(p)) + ")";
+}
+
+void debug_out() { cerr << endl; }
+template<class H, class... T> void debug_out(const H& h, const T&... t) {
+    cerr << " " << to_string(h);
+    debug_out(t...);
+}
+#ifndef ONLINE_JUDGE
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+#else
+#define debug(...) if (0) puts("No effect.")
+#endif
+
+class SortedList {
+public:
+    std::vector<int> nums;
+
+    SortedList() {}
+
+    void add(int value) {
+        auto it = std::lower_bound(nums.begin(), nums.end(), value);
+        nums.insert(it, value);
+    }
+
+    int greaterCount(int value) {
+        return nums.end() - std::upper_bound(nums.begin(), nums.end(), value);
+    }
+
+    size_t size() const {
+        return nums.size();
+    }
+
+    int operator[](int index) const {
+        return nums[index];
+    }
+};
+
+class Solution {
+public:
+    vector<int> resultArray(vector<int>& nums) {
+        SortedList arr1, arr2;
+        arr1.add(nums[0]), arr2.add(nums[1]);
+        vector<int> res1({nums[0]}), res2({nums[1]});
+        for (int i = 2; i < nums.size(); i++) {
+            int c1 = arr1.greaterCount(nums[i]);
+            int c2 = arr2.greaterCount(nums[i]);
+            if (c1 > c2 || (c1 == c2 && res1.size() <= res2.size())) {
+                arr1.add(nums[i]);
+                res1.push_back(nums[i]);
+            }
+            else {
+                arr2.add(nums[i]);
+                res2.push_back(nums[i]);
+            }
+        }
+        res1.insert(res1.end(), res2.begin(), res2.end());
+        return res1;
+    }
+};
