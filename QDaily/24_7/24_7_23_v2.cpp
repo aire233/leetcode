8817dd64f15712794aa596101a969c9595c57255
chenyiming <chenyiming06@kuaishou.com>
2024-07-23 15:41:44 +0800

    template

--- QDaily/24_7/24_7_23_v2.cpp
+++ QDaily/24_7/24_7_23_v2.cpp
@@ -14,8 +14,8 @@ using namespace std;
 
 template <class A, class B> string to_string(const pair<A, B> &p);
 template <class A, class B, class C> string to_string(const tuple<A, B, C> &p);
-template <class A, class B, class C, class D>
-string to_string(const tuple<A, B, C, D> &p);
+template <class A, class B, class C, class dp>
+string to_string(const tuple<A, B, C, dp> &p);
 string to_string(const string &s) { return '"' + s + '"'; }
 string to_string(const char *s) { return to_string((string)s); }
 string to_string(char c) { return "'" + string(1, c) + "'"; }
@@ -43,8 +43,8 @@ template <class A, class B, class C> string to_string(const tuple<A, B, C> &p) {
   return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " +
          to_string(get<2>(p)) + ")";
 }
-template <class A, class B, class C, class D>
-string to_string(const tuple<A, B, C, D> &p) {
+template <class A, class B, class C, class dp>
+string to_string(const tuple<A, B, C, dp> &p) {
   return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " +
          to_string(get<2>(p)) + ", " + to_string(get<3>(p)) + ")";
 }
@@ -73,3 +73,52 @@ static auto _ = []() {
 ( o.o ) 
  > ^ <
 */
+
+class Solution {
+public:
+    int sumOfPowers(vi& nums, int k) {
+        int n = sz(nums); sort(all(nums)); vi vals;
+        For(i, 0, n) For(j, 0, i) {
+            vals.push_back(nums[i] - nums[j]);
+        }
+        vals.push_back(inf); sort(all(vals));
+        vals.erase(unique(all(vals)), vals.end());
+
+        vvvi dp(n, vector(k + 1, vector(sz(vals), 0)));
+        vvi border(n, vector(k + 1, 0));
+        vvi sum(k + 1, vector(sz(vals), 0));
+        vvi suf(n, vector(k + 1, 0));
+
+        For(i, 0, n) {
+            For(j, 0, i) {
+                int pos = lower_bound(all(vals), nums[i] - nums[j]) - vals.begin();
+                For(p, 1, k + 1) {
+                    while (border[j][p] < pos) {
+                        sum[p][border[j][p]] = (sum[p][border[j][p]] - suf[j][p] + mod) % mod;
+                        sum[p][border[j][p]] = (sum[p][border[j][p]] + dp[j][p][border[j][p]]) % mod;
+                        suf[j][p] = (suf[j][p] - dp[j][p][border[j][p]] + mod) % mod;
+                        border[j][p]++;
+                        sum[p][border[j][p]] = (sum[p][border[j][p]] + suf[j][p]);
+                    }
+                }
+            }
+
+            dp[i][1][sz(vals) - 1] = 1;
+            For(p, 2, k + 1) For(v, 0, sz(vals)) {
+                dp[i][p][v] = sum[p - 1][v];
+            }
+            For(p, 1, k + 1) {
+                For(v, 0, sz(vals)) {
+                    suf[i][p] = (suf[i][p] + dp[i][p][v]) % mod;
+                }
+                sum[p][0] = (sum[p][0] + suf[i][p]) % mod;
+            }
+        }
+
+        int res = 0;
+        For(i, 0, n) For(v, 0, sz(vals)) {
+            res = (res + 1ll * vals[v] * dp[i][k][v] % mod) % mod;
+        }
+        return res;
+    }
+};
